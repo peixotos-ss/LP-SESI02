a3c2f7dc5a4a089bcd92373542f10bc7da80b1df
peixotos-ss <brunolorenzet02@gmail.com>
2026-05-15 11:09:29 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -10,5 +10,26 @@ function navTo(id){
     document.getElementById(id).scrollIntoView({
         behavior:'smooth',
         block: 'start'
-    })  
+    })
 }
+
+
+const cards= document.querySelectorAll('#s2, .card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            cards.forEach((card, index)=> {
+                setTimeout(()=> {
+                    card.classList.add('visible');
+                }, index * 200);
+            });
+        }else{
+            cards.forEach((card)=>{
+                card.classList.remove('visible');
+            });
+        }
+    })
+}, {threshold: 0.2});
+
+observer.observe(document.querySelector('#s2'))  ;
